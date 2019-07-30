6ce4e81b6fca1daab9e7dc44c7efdc039c8bc70c
Lucas Homer <lucashomer@Lucass-MacBook-Pro.local>
2019-07-29 21:10:07 -0500

    import image assets

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,12 +3,16 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Smiley from "../components/smiley"
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <p>It me</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Smiley />
+    </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
