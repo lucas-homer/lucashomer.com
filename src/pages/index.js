623994d50c82398bbf2cf9da5e096fadce3f7d80
Lucas Homer <lucashomer@Lucass-MacBook-Pro.local>
2019-07-29 20:26:54 -0500

    add headshot image

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,10 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="lucas homer dot com" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to my website</p>
+
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
