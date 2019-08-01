a6093437021d4e3e2248b82504f8c682e6d2c7e3
Lucas Homer <lucashomer@Lucass-MacBook-Pro.local>
2019-07-31 20:56:55 -0500

    bring in typography and styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `lucas homer`,
+    description: `Personal site for professional web developer Lucas Homer`,
+    author: `Lucas Homer`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,14 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/LogoIconLarge.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/components/styled/Header.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const HeaderStyles = styled.header`
+  display: flex;
+  #logo {
+    width: 12%;
+    margin: 2rem;
+  }
+  #menu {
+    padding: 2.5rem;
+  }
+`
